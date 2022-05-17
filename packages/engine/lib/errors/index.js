2fb6e11eb680801f254ef5a858c3224f82c832c3
wess <wcope@me.com>
2022-05-17 12:59:15 -0400

    Cleans up build artifacts

--- packages/engine/lib/errors/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DatabaseError = void 0;
-var db_1 = require("./db");
-Object.defineProperty(exports, "DatabaseError", { enumerable: true, get: function () { return __importDefault(db_1).default; } });
-//# sourceMappingURL=index.js.map
